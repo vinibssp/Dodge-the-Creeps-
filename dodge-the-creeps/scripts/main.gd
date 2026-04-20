4746bffc5469663fa20e9adb2e8daef0e7887434
Vinicius Souza <vinibssp@gmail.com>
2026-04-20 12:25:39 -0300

    fim

--- dodge-the-creeps/scripts/main.gd
+++ dodge-the-creeps/scripts/main.gd
@@ -3,19 +3,23 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func _ready():
-	new_game()
-	
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	#$Music.stop()
+	#$DeathSound.play()
 
 func new_game():
+	get_tree().call_group(&"mobs", &"queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
+	
 func _on_mob_timer_timeout() -> void:
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
@@ -44,7 +48,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
-	print(score)
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
